3d0cc4975ff8bb59f636adc2f9623abdf9ee30dc
abhigupta0507 <abhishekgupta39585@gmail.com>
2024-09-18 00:18:38 +0530

    created project folders

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./components/Home.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <Home />
+      </header>
+    </div>
+  );
+}
