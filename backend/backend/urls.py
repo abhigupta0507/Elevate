9515fb45e506b8e96735ee465331350b1d815850
Anna Narang <annanarang13@gmail.com>
2024-10-25 12:57:16 +1100

    diet

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('api/users/', include('users.urls')), 
     path('api/community/', include('community.urls')),
     path('api/workouts/', include('workouts.urls')),
-<<<<<<< HEAD
     path('api/diet/', include('diet.urls')),  
-]
-=======
     path('api/progress/',include('progress.urls')),
     path('api/badges/',include('badges.urls')),
 ]
->>>>>>> 7699ff9d94420b6e68367cf8adb0687cac95e578
