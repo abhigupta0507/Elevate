82637f0325716109d2d0302c4f2bdd3a75300f42
Abhishek Gupta <152425019+abhigupta0507@users.noreply.github.com>
2025-03-01 16:37:00 +0530

    fix

--- 1.py
+++
@@ -1,8 +0,0 @@
-from django.core.mail import send_mail
-send_mail(
-    'Test Email',
-    'This is a test message.',
-    'Elevate <abhishekgupta39585@gmail.com>',
-    ['abhishekguptakherla@gmail.com'],  # Replace with your email address for testing
-    fail_silently=False,
-)
