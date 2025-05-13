ada6f17be7ae38b81747145d03571316c1b5c87e
Abhishek Gupta <abhishekgupta39585@gmail.com>
2025-05-14 00:59:56 +0530

    deployed database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,13 +94,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE'))
+}
 
 
 
